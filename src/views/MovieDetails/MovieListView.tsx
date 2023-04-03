7277ff6afdc0bf5c5e3919a743b6d2f0e863f705
Peti <kpeti962@gmailcom>
2023-04-03 20:38:20 +0200

    Projekt létrehozása

---
+++ src/views/MovieDetails/MovieListView.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MovieListView = () => {
+  return (
+    <div>MovieListView</div>
+  )
+}
+
+export default MovieListView;
---
+++ src/views/Movies/MovieDetailsView.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MovieDetailsView = () => {
+  return (
+    <div>MovieDetailsView</div>
+  )
+}
+
+export default MovieDetailsView;
