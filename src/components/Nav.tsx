59ad57e77a2533d28d534e04df3f8d46369d9398
Peti <kpeti962@gmailcom>
2023-04-30 10:01:07 +0200

    feat/ML-10 movieDetails page better look

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -38,36 +38,39 @@ const Nav = () => {
           <img className="w-80 cursor-pointer" src={logo} alt="logo" />
         </Link>
       </div>
-      <div className="flex justify-between md:hidden">
-        <button
-          className="p-2 rounded-md text-gray-700 hover:cursor-pointer text-2xl"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <GiHamburgerMenu/>
-        </button>
-      </div>
-      <div
-        className={`${
-          isOpen ? "block" : "hidden"
-        } md:flex justify-evenly mt-4`}
-      >
-        {navBarPieces.map((item) => (
-          <div
-            key={item.path}
-            className="cursor-pointer p-2 hover:bg-gray-200 ease-in duration-300 font-bold"
+      <div>
+        <div className="flex justify-between md:hidden">
+          <button
+            className="p-2 rounded-md text-gray-700 hover:cursor-pointer text-2xl"
+            onClick={() => setIsOpen(!isOpen)}
           >
-            <Link to={item.path}>{t(item.name)}</Link>
-          </div>
-        ))}
-        <Link to={"search-page"}>
-          <div className="text-xl cursor-pointer p-1 hover:bg-gray-200 ease-in duration-300 font-bold">
-            {t("Search")}
-          </div>
-        </Link>
-        <select onChange={(e) => i18n.changeLanguage(e.target.value)}>
-          <option value="hu">HU</option>
-          <option value="en">EN</option>
-        </select>
+            <GiHamburgerMenu />
+          </button>
+        </div>
+        <div
+          /* Itt meg kellene oldani, hogyha kicsiben kinyitom a menüt, utána felnyagyítom, akkor a fixed és bg értékek megmaradnak */
+          className={`${
+            isOpen ? "block bg-gray-700 bg-opacity-90  fixed w-full" : "hidden"
+          } text-center md:flex md:justify-evenly mt-4`}
+        >
+          {navBarPieces.map((item) => (
+            <div
+              key={item.path}
+              className="cursor-pointer p-2 hover:bg-gray-200 ease-in duration-300 font-bold"
+            >
+              <Link to={item.path}>{t(item.name)}</Link>
+            </div>
+          ))}
+          <Link to={"search-page"}>
+            <div className="text-xl cursor-pointer p-1 hover:bg-gray-200 ease-in duration-300 font-bold">
+              {t("Search")}
+            </div>
+          </Link>
+          <select onChange={(e) => i18n.changeLanguage(e.target.value)}>
+            <option value="hu">HU</option>
+            <option value="en">EN</option>
+          </select>
+        </div>
       </div>
     </div>
   );
